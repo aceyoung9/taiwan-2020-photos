1f2f825142f5f76caabfcda052b705c7f69fa76d
Ace Young <ace.young289@gmail.com>
2019-12-30 23:19:14 +0800

    Update About Page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import {
-  HeaderBack,
-  HeadingXL,
-  Layout,
-  SEO,
-  TextBody,
-} from '../components';
+import { HeaderBack, HeadingXL, Layout, SEO, TextBody } from '../components';
 
 const About = ({ data }) => {
   return (
@@ -16,8 +10,17 @@ const About = ({ data }) => {
       <Layout>
         <HeadingXL>About</HeadingXL>
         <TextBody as="div">
-          <p>This is a little site to hold some travel photos.</p>
-          <p>It is strongly based off of <a href='https://lewis-gatsby-starter-blog.netlify.com/'>the Lewis Gatsby Starter</a></p>
+          <p>
+            This is a little site to hold some travel photos. I can normally be
+            found at <a href="//aceyoung.xyz">aceyoung.xyz</a>
+          </p>
+          <p>
+            The site design is strongly based off of the{' '}
+            <a href="https://lewis-gatsby-starter-blog.netlify.com/">
+              Lewis Gatsby Starter
+            </a>
+            .
+          </p>
           <p>Daily entries may be backfilled.</p>
         </TextBody>
       </Layout>
